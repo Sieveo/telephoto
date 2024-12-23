20835f4fe678a1da98d748f41398e0e26e0bad8b
Saket Narayan <saket@saket.me>
2024-12-23 13:23:26 -0500

    Add kotlin multiplatform plugin to :sub-sampling-image

--- zoomable-image/sub-sampling-image/build.gradle.kts
+++ zoomable-image/sub-sampling-image/build.gradle.kts
@@ -1,37 +1,47 @@
 plugins {
   id("me.saket.android.library")
   id("me.saket.library.publishing")
-  id("me.saket.kotlin.android")
-  id("me.saket.compose")
+  id("me.saket.kotlin.multiplatform")
   id("me.saket.android.test")
   alias(libs.plugins.kotlin.parcelize)
   alias(libs.plugins.paparazzi)
 }
-
 android {
   namespace = "me.saket.telephoto.subsamplingimage"
   buildFeatures {
     buildConfig = true
   }
 }
-
-dependencies {
-  api(projects.zoomable)
-
-  api(libs.okio.core)
-  api(libs.compose.foundation)
-  implementation(libs.compose.ui.util)
-  implementation(libs.androidx.ktx.core)
-  implementation(libs.androidx.exif)
-  implementation(libs.androidx.tracing)
-  implementation(libs.kotlinx.immutableCollections)
-
-  testImplementation(libs.junit)
-  testImplementation(libs.assertk)
-  testImplementation(libs.testParamInjector)
-  testImplementation(libs.kotlinx.coroutines.test)
-  testImplementation(libs.turbine)
-  testImplementation(libs.robolectric)
-
-  androidTestImplementation(projects.testUtil)
+kotlin {
+  sourceSets {
+    commonMain.dependencies {
+      api(projects.zoomable)
+      api(libs.okio.core)
+      api(compose.foundation)
+      implementation(compose.uiUtil)
+      implementation(libs.kotlinx.immutableCollections)
+    }
+    androidMain {
+      dependencies {
+        implementation(libs.androidx.ktx.core)
+        implementation(libs.androidx.exif)
+        implementation(libs.androidx.tracing)
+      }
+    }
+    androidUnitTest {
+      dependencies {
+        implementation(libs.junit)
+        implementation(libs.assertk)
+        implementation(libs.testParamInjector)
+        implementation(libs.kotlinx.coroutines.test)
+        implementation(libs.turbine)
+        implementation(libs.robolectric)
+      }
+    }
+    androidInstrumentedTest {
+      dependencies {
+        implementation(projects.testUtil)
+      }
+    }
+  }
 }
